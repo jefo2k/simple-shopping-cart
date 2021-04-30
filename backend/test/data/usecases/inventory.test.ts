787e43d4a8a2d1ad036c3f983ed8c146cc705a6d
Jeferson Santos <jefo2k@gmail.com>
2021-04-30 12:15:54 -0300

    Ensure that method save will be called once an inventory item is added

--- backend/test/data/usecases/inventory.test.ts
+++ backend/test/data/usecases/inventory.test.ts
@@ -2,19 +2,27 @@ import { InventoryManager } from "../../../src/domain/usecases"
 import { InventoryItem } from '../../../src/domain/entities/inventory';
 
 interface InventoryStore {
-
+  save: () => void
 }
 
 class InventoryStoreSpy implements InventoryStore {
   addCallsCount = 0
   loadCallsCount = 0
+
+  save (): void {
+    this. addCallsCount++
+  }
 }
 
 class LocalInventoryManager implements InventoryManager {
   constructor(
-    private readonly _inventoryStore: InventoryStore
+    private readonly inventoryStore: InventoryStore
   ) {}
 
+  async save(): Promise<void> {
+    this.inventoryStore.save()
+  }
+  
   add(_: InventoryItem) {}
   
   load() {}
@@ -28,6 +36,15 @@ describe('Inventory manager tests', () => {
 
     // inventoryManager.add(new InventoryItem('product1', 10))
     expect(inventoryStore.addCallsCount).toBe(0)
+  })
+
+  it('should call save once when an inventory item is added', async () => {
+    const inventoryStore = new InventoryStoreSpy()
+    const sut = new LocalInventoryManager(inventoryStore)
+    await sut.save()
+
+    // inventoryManager.add(new InventoryItem('product1', 10))
+    expect(inventoryStore.addCallsCount).toBe(1)
   })
 
 })
